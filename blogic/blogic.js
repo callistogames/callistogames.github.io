d3c67d0b042cb6b92e336a4a86de3f791b9a9c64
Brent Bessemer <bmb0610@gmail.com>
2014-05-26 20:34:51 -0400

    Update blogic.js

--- blogic/blogic.js
+++ blogic/blogic.js
@@ -43,38 +43,41 @@
 			console.log(e);
 		}
 	}
-	
+
 	// Create the local copy of the Blogic library.
 	var bl = new Object();
-	
+
 	// Load the preferences file.
 	r = new XMLHttpRequest();
 	r.open("GET", dir+"/.blogic/preferences.json", false);
 	r.send();
 	bl.prefs = JSON.parse(r.responseText);
-	
+
 	// Basic function: post.
 	bl.post = function (url, container, options) {
 		if (bl.prefs.timeZone == "local") {
-			date = new Date(options.date.UTC()+(new Date(0)).setUTCMinutes(-(new Date()).getTimeZoneOffset())));
+			date = new Date(options.date.UTC()+(new Date(0)).setUTCMinutes(-(new Date()).getTimeZoneOffset()));
 		} else {
-			date = new Date(options.date.UTC()+(new Date(0)).setUTCMinutes(bl.prefs.timeZone.offset)));
+			date = new Date(options.date.UTC()+(new Date(0)).setUTCMinutes(bl.prefs.timeZone.offset));
 		}
 		switch (bl.prefs.dateFormat) {
 			case "dmy":
 				dateString = bl.prefs.days[date.getUTCDay()] + date.getUTCDate() + bl.prefs.months[date.getUTCMonth()] + date.getUTCFullYear();
-			case "mdy":
-				dateString = bl.prefs.days[date.getUTCDay()] + bl.prefs.months[date.getUTCMonth()] + date.getUTCDate() + date.getUTCFullYear();
-			default:
+				break;
+      case "mdy":
+        dateString = bl.prefs.days[date.getUTCDay()] + bl.prefs.months[date.getUTCMonth()] + date.getUTCDate() + date.getUTCFullYear();
+				break;
+      default:
 				dateString = date.toISOString();
 		}
 		switch (bl.prefs.timeFormat) {
 			case "24":
 				timeString = date.getUTCHours() + ':' + date.getUTCMinutes() + bl.prefs.timeZone.name;
-			default:
+				break;
+      default:
 				timeString = "";
 		}
-		
+
 		$.get(url, function (content) {
 			$post = $('<div class="-b-post"></div>');
 			raw = $.parseHTML(content);
@@ -99,8 +102,8 @@
 			}
 			$post.prependTo(container);
 		});
-	}
-	
+	};
+
 	bl.postList = function (url, container) {
 		$.get(dir+url, function (list) {
 			//var list = $.parseXML(raw);
@@ -115,10 +118,10 @@
 				bl.post(dir+postUrl, container, opts);
 			}
 		});
-	}
-	
+	};
+
 	// Add the library and an alias to the window object.
 	window.blog = bl;
 	window._b = bl;
-	
+
 })('.');
